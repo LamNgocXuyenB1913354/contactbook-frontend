613757ae65b3a58b19eca03610fb66b420250cca
unknown <xuyenb1913354@example.com>
2022-11-04 11:36:37 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,10 @@ const routes = [
         name: "contactbook", 
         component: ContactBook, 
     }, 
+    { 
+        path: "/:pathMatch(.*)*", 
+        name: "notfound", 
+        component: () => import("@/views/NotFound.vue"), },
 ];
 
 const router = createRouter({
